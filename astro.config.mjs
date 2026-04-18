279779daf1792166b01656237a21a40413ed5aa7
Mesbah Mowlavi <powerofm@users.noreply.github.com>
2026-04-17 22:36:58 -0700

    Pairing session with mom

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://escolaportuguesavancouver.ca',
+  vite: {
+    server: {
+      allowedHosts: 'all',
+    },
+  },
 });
